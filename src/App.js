b427b4d975b2a793f73fa4aaec3580f1bb625ec8
Abdi Elman <abdi.elman29@gmail.com>
2024-09-19 17:49:52 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import MultiStepForm from "./multistepform/MultiStepForm";
 
+import "./App.css"
 function App() {
   return (
-    <div className="App">
-        <MultiStepForm />
-    </div>
+      <div className="App">
+          <div className="jumbotron jumbotron-fluid jumbo-header">
+              <div className="container">
+                  <h1 className="display-3">Hello, world!</h1>
+                  <p>This is a template for a simple marketing or informational website. It includes a large callout
+                      called a jumbotron and three supporting pieces of content. Use it as a starting point to create
+                      something more unique.</p>
+              </div>
+          </div>
+          <MultiStepForm/>
+      </div>
   );
 }
 
